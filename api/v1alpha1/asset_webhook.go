e5928611a69a40edca13d23bd7191653fe13072c
yckao <yckaokarl@gmail.com>
2024-02-13 12:56:00 +0800

    chore: upgrade dependencies and go version

--- api/v1alpha1/asset_webhook.go
+++ api/v1alpha1/asset_webhook.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"context"
 	"encoding/json"
 
 	"github.com/rueian/kinko/kms"
@@ -24,6 +25,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 // log is for logging in this package.
@@ -37,48 +39,49 @@ func (r *Asset) SetupWebhookWithManager(mgr ctrl.Manager) error {
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
-// +kubebuilder:webhook:path=/mutate-seals-kinko-dev-v1alpha1-asset,mutating=true,failurePolicy=fail,groups=seals.kinko.dev,resources=assets,verbs=create;update,versions=v1alpha1,name=masset.kb.io
+// +kubebuilder:webhook:path=/mutate-seals-kinko-dev-v1alpha1-asset,mutating=true,failurePolicy=fail,groups=seals.kinko.dev,resources=assets,verbs=create;update,versions=v1alpha1,name=masset.kb.io,admissionReviewVersions=v1,sideEffects=NoneOnDryRun
 
-var _ webhook.Defaulter = &Asset{}
+var _ webhook.CustomDefaulter = &Asset{}
 
-// Default implements webhook.Defaulter so a webhook will be registered for the type
-func (r *Asset) Default() {
+// Default implements webhook.CustomDefaulter so a webhook will be registered for the type
+func (r *Asset) Default(ctx context.Context, obj runtime.Object) error {
 	assetlog.Info("default", "name", r.Name)
 
 	// TODO(user): fill in your defaulting logic.
-}
-
-func (r *Asset) Validate() error {
-	if r.Spec.SealingPlugin == "GCP" {
-		param := kms.GCPParams{}
-		if err := json.Unmarshal([]byte(r.Spec.SealingParams), &param); err != nil {
-			return err
-		}
-		return param.Validate()
-	}
 	return nil
 }
 
+func (r *Asset) Validate() (admission.Warnings, error) {
+	if r.Spec.SealingPlugin == "GCP" {
+		param := kms.GCPParams{}
+		if err := json.Unmarshal([]byte(r.Spec.SealingParams), &param); err != nil {
+			return nil, err
+		}
+		return param.Validate()
+	}
+	return nil, nil
+}
+
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-// +kubebuilder:webhook:verbs=create;update,path=/validate-seals-kinko-dev-v1alpha1-asset,mutating=false,failurePolicy=fail,groups=seals.kinko.dev,resources=assets,versions=v1alpha1,name=vasset.kb.io
+// +kubebuilder:webhook:verbs=create;update,path=/validate-seals-kinko-dev-v1alpha1-asset,mutating=false,failurePolicy=fail,groups=seals.kinko.dev,resources=assets,versions=v1alpha1,name=vasset.kb.io,admissionReviewVersions=v1,sideEffects=NoneOnDryRun
 
 var _ webhook.Validator = &Asset{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
-func (r *Asset) ValidateCreate() error {
+func (r *Asset) ValidateCreate() (admission.Warnings, error) {
 	assetlog.Info("validate create", "name", r.Name)
 	return r.Validate()
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
-func (r *Asset) ValidateUpdate(old runtime.Object) error {
+func (r *Asset) ValidateUpdate(old runtime.Object) (admission.Warnings, error) {
 	assetlog.Info("validate update", "name", r.Name)
 	return r.Validate()
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
-func (r *Asset) ValidateDelete() error {
+func (r *Asset) ValidateDelete() (admission.Warnings, error) {
 	assetlog.Info("validate delete", "name", r.Name)
 	// TODO(user): fill in your validation logic upon object deletion.
-	return nil
+	return nil, nil
 }
